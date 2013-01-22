b4c2cb58f10c5e60342633f5b5477453bbaf4ccb
Anders Arpteg <anders@campanja.com>
2013-01-22 18:24:15 +0100

    Updated active.py

--- active.py
+++ active.py
@@ -1,6 +1,6 @@
 import learners.svm_learners.simple_svm_learner as simple_svm_learner
 
-def online_svm(self, training_data):
+def active_svm_learner(self, training_data):
     my_data = build_dataset_from_file(training_data)
     learner = simple_svm_learner.SimpleLearner(my_data)
     learner.label_instances(learner.pick_balanced_initial_training_set(1))
